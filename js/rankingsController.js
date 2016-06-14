b0cf943df54c93d7677cf786ec95bdd24b399e39
Sivan Patel <sivan.patel@googlemail.com>
2016-06-14 19:42:03 +0100

    Calculating point difference correctly

--- js/rankingsController.js
+++ js/rankingsController.js
@@ -1,3 +1,17 @@
 rankingsTable.controller('rankingsController', [function() {
-  
+
+  var self = this
+
+  self.rankingsData = [
+                        { "team": { "name": "Australia", "id": 32 }, "pos": 1, "pts": 54.23 },
+                        { "team": { "name": "New Zealand", "id": 62 }, "pos": 2, "pts": 54.00 },
+                        { "team": { "name": "France", "id": 2 }, "pos": 3, "pts": 52.95 },
+                        { "team": { "name": "England", "id": 1 }, "pos": 4, "pts": 52.32 },
+                        { "team": { "name": "Romania", "id": 24 }, "pos": 5, "pts": 43.50 }
+                      ]
+
+  self.pointDifference = function(homeTeamPoints, awayTeamPoints) {
+      return (homeTeamPoints + 3) - awayTeamPoints
+  }
+
 }])
--- test/unit/rankingsController.spec.js
+++ test/unit/rankingsController.spec.js
@@ -8,4 +8,11 @@ describe('rankingsController', function() {
     ctrl = $controller('rankingsController')
   }))
 
+  it('correctly calculates the point difference when the home team has a higher score', function() {
+    expect(ctrl.pointDifference(10, 9)).toEqual(4)
+  })
+
+  it('correctly calculates the point difference when the away team has a higher score', function() {
+    expect(ctrl.pointDifference(2, 6)).toEqual(-1)
+  })
 })
